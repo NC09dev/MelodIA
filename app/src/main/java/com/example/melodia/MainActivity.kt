ed0b2c1fc447ea7772b1cdfb4e3fa99b2239c840
canon <canongaitan4@gmail.com>
2025-04-03 11:23:14 -0500

    create_bodyxml

---
+++ app/src/main/java/com/example/melodia/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.melodia
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Forzar modo claro
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        // Habilitar bordes sin recortes
+        enableEdgeToEdge()
+
+        setContentView(R.layout.body_activity)
+
+        // Ajustar los insets del sistema
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
